e8b9e9e31a147565f513f857bdfceca9f03bc96d
ronikstha <erosronik123@gmail.com>
2020-05-26 19:00:35 +1000

    Profile UI finished/ cart page still not finished

--- screens/CartScreen.js
+++ screens/CartScreen.js
@@ -5,6 +5,8 @@ import Ionicons from "react-native-vector-icons/Ionicons";
 import { AsyncStorage } from 'react-native';
 var { width } = Dimensions.get("window")
 
+//35.57
+
 export default class CartScreen extends React.Component {
 
   constructor(props) {
@@ -14,77 +16,99 @@ export default class CartScreen extends React.Component {
     };
   }
 
-  componentDidMount()
-  {
-    AsyncStorage.getItem('cart').then((cart)=>{
+  componentDidMount() {
+    AsyncStorage.getItem('cart').then((cart) => {
       if (cart !== null) {
         // We have data!!
         const cartfood = JSON.parse(cart)
-        this.setState({dataCart:cartfood})
+        this.setState({ dataCart: cartfood })
       }
     })
-    .catch((err)=>{
-      alert(err)
-    })
+      .catch((err) => {
+        alert(err)
+      })
+  }
+
+  onChangeQual(i, type) {
+    const dataCar = this.state.dataCart
+    let cantd = dataCar[i].quantity;
+
+    if (type) {
+      cantd = cantd + 1
+      dataCar[i].quantity = cantd
+      this.setState({ dataCart: dataCar })
+    }
+    else if (type == false && cantd >= 2) {
+      cantd = cantd - 1
+      dataCar[i].quantity = cantd
+      this.setState({ dataCart: dataCar })
+    }
+    else if (type == false && cantd == 1) {
+      dataCar.splice(i, 1)
+      this.setState({ dataCart: dataCar })
+    }
   }
 
   render() {
     return (
       <View style={{ flex: 1, alignItems: 'center' }}>
-         <ScrollView>
-         {
-           this.state.dataCart.map((item)=>{
-               return(
-                <View style={{width:width-20,margin:10,backgroundColor:'transparent', flexDirection:'row', borderBottomWidth:2, borderColor:"#cccccc", paddingBottom:10}}>
-                <Image resizeMode={"contain"} style={{width:width/3,height:width/3}} source={require('../assets/images/cap.png')} />
-                   <View style={{flex:1, backgroundColor:'transparent', padding:10, justifyContent:"space-between"}}>
-                     <View>
-                       <Text style={{fontWeight:"bold", fontSize:20}}>Cappaccino</Text>
-                       <Text>Description</Text>
-                     </View>
-                     <View style={{flexDirection:'row',justifyContent:'space-between'}}>
-                       <Text style={{fontWeight:'bold',color:"#FF914D",fontSize:20}}>$4.5</Text>
-                       <View style={{flexDirection:'row', alignItems:'center'}}>
-                         <TouchableOpacity>
-                           <Ionicons name="ios-remove-circle" size={30} color={"#FF914D"} />
-                         </TouchableOpacity>
-                         <Text style={{paddingHorizontal:8, fontWeight:'bold'}}>5</Text>
-                         <TouchableOpacity>
-                           <Ionicons name="ios-add-circle" size={30} color={"#FF914D"} />
-                         </TouchableOpacity>
-                       </View>
-                     </View>
-                   </View>
-                 </View>
-               )
-           })
-         }  
-         </ScrollView>
+        <ScrollView>
+          {
+            this.state.dataCart.map((item, i) => {
+              return (
+                <View style={{ width: width - 20, margin: 10, backgroundColor: 'transparent', flexDirection: 'row', borderBottomWidth: 2, borderColor: "#cccccc", paddingBottom: 10 }}>
+                  <Image resizeMode={"contain"} style={{ width: width / 3, height: width / 3 }} source={item.image} />
+                  <View style={{ flex: 1, backgroundColor: 'trangraysparent', padding: 10, justifyContent: "space-between" }}>
+                    <View>
+              <Text style={{ fontWeight: "bold", fontSize: 20 }}>aa</Text>
 
-         
+                      <Text>{JSON.stringify(item)}</Text>
+                    </View>
+                    <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+                      <Text style={{ fontWeight: 'bold', color: "#FF914D", fontSize: 20 }}>{item.price}</Text>
+                      <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                        <TouchableOpacity>
+                          <Ionicons name="ios-remove-circle" size={30} color={"#FF914D"} />
+                        </TouchableOpacity>
+                        <Text style={{ paddingHorizontal: 8, fontWeight: 'bold', fontSize: 18 }}>1</Text>
+                        <TouchableOpacity>
+                          <Ionicons name="ios-add-circle" size={30} color={"#FF914D"} />
+                        </TouchableOpacity>
+                      </View>
+                    </View>
+                  </View>
+                </View>
+              )
+            })
+          }
+        </ScrollView>
+        <View style={{ height: 20 }} />
+        <TouchableOpacity style={{
+          backgroundColor: "#FF914D",
+          alignItems: 'center',
+          padding: 10,
+          width: width - 40,
+          borderRadius: 5
+        }}>
+          <Text style={{
+            fontSize: 24,
+            fontWeight: "bold",
+            color: 'white'
+          }}>
+            CHECKOUT
+          </Text>
+        </TouchableOpacity>
+        <View style={{ height: 10 }} />
+      </View>
 
-          <View style={{ height: 20 }} />
-          <TouchableOpacity style={{
-              backgroundColor: "#FF914D",
-              alignItems: 'center',
-              padding: 10,
-              width: width-40,
-              borderRadius: 5
-              }}>
-              <Text style={{
-                fontSize: 24,
-                fontWeight: "bold",
-                color: 'white'
-              }}>
-                CHECKOUT
-              </Text>
-            </TouchableOpacity>
-            <View style={{ height: 10 }} />
-        </View>
-    );
+
+
+
+    )
+
   }
 
-  
+
 }
 
 
